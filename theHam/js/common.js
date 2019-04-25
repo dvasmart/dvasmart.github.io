186563f7c1e1fb322937ddc89280c47992d73099
Dronskiy Vitaliy <dvasmart@gmail.com>
2019-04-25 03:40:08 +0300

    theHam - make tabs in about block

--- theHam/js/common.js
+++ theHam/js/common.js
@@ -1,3 +1,17 @@
+// about-main
+(function($) {
+    $(function() {
+     
+      $('ul.about-tab__nav').on('click', 'li:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('div.about-tab').find('div.about-tab__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
+     
+    });
+    })(jQuery);
+
+// portfolio
 $(function(){
     $('.portfolio-tabs button').click(function(){
         var get_id = this.id;
