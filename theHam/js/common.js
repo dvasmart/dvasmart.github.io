728f418bfa2afb91e6bbe9c7d6b22ccd14eb32fb
Dronskiy Vitaliy <dvasmart@gmail.com>
2019-05-03 09:34:40 +0300

    theHam - add animated counters in testimonial block

--- theHam/js/common.js
+++ theHam/js/common.js
@@ -52,6 +52,27 @@ $('.slider-nav').slick({
     centerMode: false,
     focusOnSelect: true,
     arrows: true,
-//     autoplay: true,
-//   autoplaySpeed: 3000,
+    autoplay: true,
+    autoplaySpeed: 3000,
+});
+
+// testimonial - 4 statistic buttons
+$(document).ready(function(){
+    var show = true;
+    var countbox = "#testimonial";
+    $(window).on("scroll load resize", function(){
+    if(!show) return false;                   
+    var w_top = $(window).scrollTop();        
+    var e_top = $(countbox).offset().top;     
+    var w_height = $(window).height();        
+    var d_height = $(document).height();      
+    var e_height = $(countbox).outerHeight(); 
+    if(w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+    $(".spincrement").spincrement({
+    thousandSeparator: "",
+    duration: 3000
+    });
+    show = false;
+    }
+    });
 });
